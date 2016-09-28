cea532c1b5b4888eb134f5a529f9feff5e076906
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-09-27 18:03:04 -0700

    Started GUI work

--- src/com/ttt/model/TicTacToe.java
+++ src/com/ttt/model/TicTacToe.java
@@ -1,6 +1,17 @@
 package com.ttt.model;
+
+import javax.swing.JFrame;
+
+import com.ttt.view.RenderService;
+
 public class TicTacToe {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Board board = new Board();
+
+		JFrame frame = new JFrame("Tic Tac Toe");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new RenderService(board).getContents());
+		frame.setVisible(true);
+		frame.setSize(RenderService.PANEL_WIDTH - 1, RenderService.PANEL_HEIGHT + frame.getInsets().top - 1);
 	}
 }
