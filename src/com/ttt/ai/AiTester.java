3df55ec9174dbec0924b48b9aba3672721ff2c65
Michaelmvv <mikey@Michaels-MacBook-Pro.local>
2016-10-05 18:51:46 -0700

    Test

--- src/com/ttt/ai/AiTester.java
+++ src/com/ttt/ai/AiTester.java
@@ -2,16 +2,16 @@ package com.ttt.ai;
 
 public class AiTester {
 	static int tb[][] = {
-			{-1,0,0,0,0},
-			{-1,0,0,0,0},
-			{-1,0,0,0,0},
-			{-1,0,0,0,0},
-			{-1,0,0,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
+			{0,0,1,0,0},
 			};
 
 	public static void main(String[] args) {
 		TTTSim sim = new TTTSim(3);
 		sim.setBoard(tb);
-		System.out.println(sim.isWinner(-1));
+		System.out.println(sim.isWinner(1));
 	}
 }
