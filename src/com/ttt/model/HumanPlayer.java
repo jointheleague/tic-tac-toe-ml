158519e25077e4aa362920b882a7bf758d2ca424
matthew86707 <matt86707@Gmail.com>
2016-10-12 09:08:20 -0700

    Simple Structure

---
+++ src/com/ttt/model/HumanPlayer.java
@@ -0,0 +1,9 @@
+package com.ttt.model;
+
+public class HumanPlayer extends Player{
+
+	public HumanPlayer(String label) {
+		super(label);
+	}
+
+}
---
+++ src/com/ttt/model/Player.java
@@ -0,0 +1,19 @@
+package com.ttt.model;
+
+public class Player {
+	
+	String playerLabel;
+
+	public Player(String label){
+		this.playerLabel = label;
+	}
+	
+	public String getLabel(){
+		return playerLabel;
+	}
+	
+	public void performTurn(){
+		
+	}
+
+}
