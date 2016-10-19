42bc9398e404ae47608b97b19e38e46c29f6d1e7
Nicholas Clark <nicholas@californiaclarks.com>
2016-10-18 17:42:39 -0700

    Implementing New NeuralNetwork

--- src/com/ttt/ai/GeneticAlgorithm.java
+++ src/com/ttt/ai/GeneticAlgorithm.java
@@ -5,11 +5,11 @@ public class GeneticAlgorithm {
 	public static void main(String[] args) {
 		NeuralNetwork base = new NeuralNetwork();
 		
-		base.addLayer(new Layer(9));
-		base.addLayer(new Layer(10));
-		base.addLayer(new Layer(9));
-		
-		base.makeWeightGroups();
+		base.setInputLayer(new Layer(new Neuron[] { new Neuron(1), new Neuron(1), new Neuron(1)}));
+		base.addLogicLayer(new Layer(new Neuron[] { new Neuron(0), new Neuron(0), new Neuron(0)}));
+		base.setOutputLayer(new Layer(new Neuron[] { new Neuron(0)}));
+
+		base.connectSynapsesBetweenLayers();
 		
 		Population p = new Population(base, 200, 0.01);
 		double highestFit = p.draw();
