61ae4e703bbd61565cf6e9546a41054bb952d142
Ruoya Yan <ruoyayan@gmail.com>
2016-10-25 16:34:32 -0700

    Board Unit Tests Made
    Made and Finished!

--- src/com/ttt/test/BoardTest.java
+++ src/com/ttt/test/BoardTest.java
@@ -3,6 +3,7 @@ package com.ttt.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.ttt.model.Board;
 import com.ttt.model.Tile;
@@ -32,7 +33,44 @@ public class BoardTest {
 				}
 			}
 		}
+	}
+	
+	@Test
+	public void testBoardGetterSetter() {
+		Tile[][] tiles = new Tile[][] { { Tile.EMPTY, Tile.EMPTY, Tile.EMPTY },
+			{ Tile.EMPTY, Tile.O, Tile.EMPTY }, { Tile.EMPTY, Tile.EMPTY, Tile.EMPTY } };
+		Board b = new Board(tiles);
 		
+		assertEquals(b.getTile(1, 1), Tile.O);
+		assertEquals(b.getTile(0, 0), Tile.EMPTY);
+		assertEquals(b.getTile(-1, 0), null);
+		b.setTile(5, 5, Tile.EMPTY);
+	}
+
+	@Test
+	public void testBoardCheckWin() {
+		//TODO: Remove printout from checkWin method
+		Tile[][] tiles = new Tile[][] { { Tile.EMPTY, Tile.EMPTY, Tile.EMPTY },
+			{ Tile.EMPTY, Tile.EMPTY, Tile.EMPTY }, { Tile.EMPTY, Tile.EMPTY, Tile.EMPTY} };
+		Board b = new Board(tiles);
+		assertEquals(b.checkWin(Tile.X) || b.checkWin(Tile.O), false);
+		
+		tiles = new Tile[][] { { Tile.X, Tile.O, Tile.X },
+			{ Tile.EMPTY, Tile.O, Tile.X }, { Tile.O, Tile.EMPTY, Tile.O } };
+		b = new Board(tiles);
+		assertEquals(b.checkWin(Tile.X) || b.checkWin(Tile.O), false);
+		
+		tiles = new Tile[][] { { Tile.X, Tile.O, Tile.O},
+			{ Tile.X, Tile.X, Tile.X }, { Tile.O, Tile.X, Tile.O } };
+		b = new Board(tiles);
+		assertEquals(b.checkWin(Tile.X), true);
+		assertEquals(b.checkWin(Tile.O), false);
+		
+		tiles = new Tile[][] { { Tile.O, Tile.O, Tile.O},
+			{ Tile.O, Tile.O, Tile.O }, { Tile.O, Tile.O, Tile.O } };
+		b = new Board(tiles);
+		assertEquals(b.checkWin(Tile.X), false);
+		assertEquals(b.checkWin(Tile.O), true);
 	}
 
 }
